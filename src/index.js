afcdc53e226d42dfd0c8315f84aeed05168032c9
Mondo <mondogao@gmail.com>
2017-10-29 18:29:16 +0800

    feat: finish load logic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,47 @@
 import $ from 'zepto';
+import preloader from 'preloader';
 
 import 'normalize.css';
 
 import './index.css';
 
+const loader = preloader({
+  xhrImages: false,
+});
+const loadingStrip = $('#loadingStrip');
+
+loader.on('progress', (progress) => {
+  loadingStrip.css('left', `${-(1 - progress) * 100}%`);
+});
+loader.on('complete', () => {
+  loadingStrip.css('left', '0');
+
+  setTimeout(() => {
+    $('#load').removeClass('show').addClass('finish');
+    $('#home').addClass('show');
+  }, 500);
+
+});
+
+fetch('/manifest.json')
+  .then(data => data.json())
+  .then((data) => {
+    Object.values(data).forEach(url => loader.add(url));
+    loader.load();
+  });
+
+function switchNextPage($page, $nextPage) {
+  $page.removeClass('show').addClass('finish');
+  $nextPage.addClass('show');
+}
+
 $(() => {
-  $('#load').removeClass('show').addClass('finish');
-  $('#home').addClass('show');
+  const pageHome = $('#home');
+  const pageQuiz = $('#quiz');
+  const pageResult = $('#result');
+  const btnStart = $('#btnStart');
+
+  btnStart.on('click', () => {
+    switchNextPage(pageHome, pageQuiz);
+  });
 });
