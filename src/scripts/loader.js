f34094df9021c67fde80eeaa0c8177952e02fddf
Mondo <mondogao@gmail.com>
2017-10-30 15:46:15 +0800

    refactor(loader): rewrite

--- src/scripts/loader.js
+++ src/scripts/loader.js
@@ -4,46 +4,62 @@ import preloader from 'preloader';
 import { switchNextPage } from './helpers';
 import { publicPath } from '../../settings';
 
-const loader = preloader({
-  xhrImages: false,
-});
-const loadingStrip = $('#loadingStrip');
 let assets = {};
+const loadingStrip = $('#loadingStrip');
 
-loader.on('progress', (progress) => {
-  loadingStrip.css('left', `${-(1 - progress) * 100}%`);
-});
-loader.on('complete', () => {
-  loadingStrip.css('left', '0');
-
-  setTimeout(switchNextPage, 500, 'load', 'home');
-
-  const musicLoader = preloader({
-    xhrImages: false,
-  });
-
-  Object.values(assets).forEach((url) => {
-    if (url.match(/\.mp3|Bg|Background/)) {
-      musicLoader.add(url);
-    }
-  });
-  musicLoader.load();
-});
-
-function load() {
+export function loadAssetsList() {
   return fetch(`${publicPath}manifest.json`)
     .then(data => data.json())
     .then((data) => {
       assets = data;
-      Object.values(data).forEach((url) => {
-        if (url.match(/\.mp3|Bg|Background/)) {
-          return;
-        }
-
-        loader.add(url);
-      });
-      loader.load();
+      return data;
     });
 }
 
-module.exports = load;
+export function isFirstLoad() {
+  return Object.keys(assets).length < 1;
+}
+
+export function createLoader(regx, include = true, toPage = 'home') {
+  return new Promise((resolve, reject) => {
+    const loader = preloader({
+      xhrImages: false,
+    });
+
+    loader.on('progress', (progress) => {
+      loadingStrip.css('left', `${-(1 - progress) * 100}%`);
+    });
+    loader.on('complete', () => {
+      loadingStrip.css('left', '0');
+
+      setTimeout(switchNextPage, 500, 'load', toPage);
+
+      resolve();
+    });
+
+    Object.values(assets).forEach((url) => {
+      if ((include && url.match(regx)) ||
+        (!include && !url.match(regx))) {
+        loader.add(url);
+      }
+    });
+    loader.load();
+  });
+}
+
+export function quizLoad(quizIds = []) {
+  const regStr = `quiz/(${quizIds.join('|')})/`;
+
+  return createLoader(new RegExp(regStr), true, 'quiz');
+}
+
+export function initLoad() {
+  if (isFirstLoad()) {
+    return loadAssetsList()
+      .then(() => {
+        createLoader(/\.mp3|Bg|Background|quiz/, false);
+      });
+  }
+
+  return createLoader(/\.mp3|Bg|Background|quiz/, false);
+}
